ac963124b4cbc105c4f198036c520d0f7bc2e713
Wanmov <1216120392@qq.com>
2024-03-15 20:49:18 +0800

    feat:登录注册页

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,61 @@
+import { Button, Checkbox, Form, Input, Space, Typography } from 'antd';
+import { UserAddOutlined } from '@ant-design/icons';
+import { REGISTER_PATHNAME } from '../../router/constans';
+import { Link } from 'react-router-dom';
+import styles from './index.module.scss';
+
+const { Title } = Typography;
+
 const Login: React.FC = () => {
-  return <></>;
+  const [form] = Form.useForm();
+
+  const onFinish = () => {};
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: false }}
+          onFinish={onFinish}
+          form={form}>
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              { type: 'string', min: 5, max: 20, message: '字符长度在 5-20 之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password" rules={[{ required: true, message: '请输入密码' }]}>
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
