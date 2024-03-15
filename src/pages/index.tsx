6febdf9facef2de3cd4da40dae8cbb3711389878
Wanmov <1216120392@qq.com>
2024-03-15 14:52:41 +0800

    style:首页样式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
+import { Button, Typography } from 'antd';
+import { MANAGE_INDEX_PATHNAME } from '../router/constans';
+import { useNavigate } from 'react-router-dom';
+import styles from './index.module.scss';
+
+const { Title, Paragraph } = Typography;
+
 const Home: React.FC = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.description}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980 份</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => navigate(MANAGE_INDEX_PATHNAME)}>
+            开始使用
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
